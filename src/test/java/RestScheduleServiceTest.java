754b67494bb58b407b68f04f945999d178ab1c5d
karina bulanova <karinasirokova5442@gmail.com>
2023-04-06 23:54:04 +0300

    change src/test

--- src/test/java/RestScheduleServiceTest.java
+++ src/test/java/RestScheduleServiceTest.java
@@ -1,30 +1,36 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.RestSchedule.RestScheduleService;
 
 public class RestScheduleServiceTest {
-    @Test
-    public void testLeaveCalculationAtLowerCost() {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    public void testLeaveCalculationAtLowerCost(int expected,int income,int expense,int threshold) {
         RestScheduleService service = new RestScheduleService();
-        int expected = 3;
-        int income = 10_000;
-        int expense = 3000;
-        int threshold = 20_000;
+     //   int expected = 3;
+     //   int income = 10_000;
+     //   int expense = 3000;
+     //   int threshold = 20_000;
         int actual = service.calculate(income, expense, threshold);
 
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    public void testCalculationOfVacationsAtHighCosts() {
-        RestScheduleService service = new RestScheduleService();
-        int expected = 2;
-        int income = 100_000;
-        int expense = 60_000;
-        int threshold = 150_000;
-        int actual = service.calculate(income, expense, threshold);
+    //  @Test
+    //public void testCalculationOfVacationsAtHighCosts() {
+    //  RestScheduleService service = new RestScheduleService();
+    //int expected = 2;
+    //int income = 100_000;
+    //int expense = 60_000;
+    //int threshold = 150_000;
+    //int actual = service.calculate(income, expense, threshold);
 
-        Assertions.assertEquals(expected, actual);
-    }
+    //Assertions.assertEquals(expected, actual);
 }
+
